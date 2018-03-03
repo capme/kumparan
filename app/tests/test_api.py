d478a09d1d9f35259ce81573db7d5635396b8d30
Herwindyo Purbo Kusumo <purbo.kusumo@acommerce.asia>
2018-03-03 19:12:27 +0700

    unit test filtering status and topic

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from ..models import News, Topic, NewsRelationTopic
 import simplejson
+import time
 
 
 class TestApi(TestCase):
@@ -68,9 +69,10 @@ class TestApi(TestCase):
         ).first()
         self.assertIsNotNone(rec_relation_news_topic)
 
-    def test_api_list_news(self):
+    def test_api_filter_news_by_status_only(self):
         self.test_api_add_news()
 
+        # test filtering status news
         resp = self.client.get('/api/news?status=DRAFT')
         self.assertEqual(resp.status_code, 200)
         self.assertNotEqual(resp.data, [])
@@ -84,6 +86,71 @@ class TestApi(TestCase):
         resp = self.client.get('/api/news/{}/delete'.format(self.rec_id_news))
         self.assertEqual(resp.status_code, 200)
 
+        resp = self.client.get('/api/news?status=DELETED')
+        self.assertEqual(resp.status_code, 200)
+        self.assertNotEqual(resp.data, [])
+        self.assertEqual(len(resp.data), 1)
+
+    def test_api_filter_news_by_topic_only(self):
+        self.test_api_add_news()
+
+        # test filtering topic news
+        resp = self.client.get('/api/news?topic=TOPIC NAME 2')
+        self.assertEqual(resp.status_code, 200)
+        self.assertNotEqual(resp.data, [])
+        self.assertEqual(len(resp.data), 1)
+
+        # create another news with different topic
+        payload = {
+            "news_title": "News Title other",
+            "news_content": "News Content other",
+            "topic_name": [
+                'Topic Name 1', 'Topic Name 3'
+            ]
+        }
+        resp = self.client.post('/api/news/add',
+                                simplejson.dumps(payload), content_type='application/json')
+        self.assertEqual(resp.status_code, 200)
+
+        # test filtering news that having TOPIC NAME 1 -> 2 news
+        resp = self.client.get('/api/news?topic=TOPIC NAME 1')
+        self.assertEqual(resp.status_code, 200)
+        self.assertNotEqual(resp.data, [])
+        self.assertEqual(len(resp.data), 2)
+
+    def test_api_filter_news_by_status_and_topic(self):
+        self.test_api_add_news()
+        time.sleep(5)
+        # create another news with different topic
+        payload = {
+            "news_title": "News Title other",
+            "news_content": "News Content other",
+            "topic_name": [
+                'Topic Name 1', 'Topic Name 3'
+            ]
+        }
+        resp = self.client.post('/api/news/add',
+                                simplejson.dumps(payload), content_type='application/json')
+        self.assertEqual(resp.status_code, 200)
+
+        # test filtering status and topic news
+        resp = self.client.get('/api/news?status=DRAFT&topic=TOPIC NAME 1')
+        self.assertEqual(resp.status_code, 200)
+        self.assertNotEqual(resp.data, [])
+        self.assertEqual(len(resp.data), 2)
+
+        # test filtering status and topic news
+        resp = self.client.get('/api/news?status=DRAFT&topic=TOPIC NAME 2')
+        self.assertEqual(resp.status_code, 200)
+        self.assertNotEqual(resp.data, [])
+        self.assertEqual(len(resp.data), 1)
+
+        # then we delete news with title "News Title"
+        # with topic : 'Topic Name 1', 'Topic Name 2'
+        resp = self.client.get('/api/news/{}/delete'.format(self.rec_id_news))
+        self.assertEqual(resp.status_code, 200)
+
+        # test filtering status deleted news
         resp = self.client.get('/api/news?status=DELETED')
         self.assertEqual(resp.status_code, 200)
         self.assertNotEqual(resp.data, [])
